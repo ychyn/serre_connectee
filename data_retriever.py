5cfc68fe65c14b6afa388a56de8d9d0825c6e704
Guillaume Coulomb <guillaume.coulomb@etu.minesparis.psl.eu>
2025-07-02 09:40:49 +0200

    code écriture csv

--- data_retriever.py
+++ data_retriever.py
@@ -1,7 +1,24 @@
 import requests
 import json
+import time
+import csv
 
 url = "http://192.168.65.176/data"
-r = requests.get(url)
-cont = r.json()
-print(type(cont))
+
+
+with open('data.csv', 'w', newline='') as fichier:
+        writer = csv.writer(fichier)
+
+noms_colonnes=["Date","Time","Temperature","Soil moisture","Air moisture"]
+
+
+while True :
+    r = requests.get(url)
+    cont = r.json()
+    time.sleep(3)
+    data = json.load(cont)
+
+    ligne=[data["Date"],data["Time"],data["Temperature"],data["Soil moisture"],data["Air moisture"]] # à raccorder avec le code arduino
+    
+    writer.writerow(ligne)
+    
